25f360a27847932ba5dc448476abbd5ac723523c
shetty manasa <manasa059shetty@gmail.com>
2026-08-09 13:58:11 +0530

    connect frontend to production API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://vervue-api.vercel.app";
 
 function App() {
   const [theme, setTheme] = useState(
